64af1eec6981a860a7f05aa8442b5181ef21eb4a
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-09-27 14:04:05 +0300

    Follow style in unet

--- reduce_model.py
+++ reduce_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class FaceDetectionModel(nn.Module):
     def __init__(self, image_width, image_height):
         super(FaceDetectionModel, self).__init__()
@@ -14,11 +15,17 @@ class FaceDetectionModel(nn.Module):
         self.fc1 = nn.Linear(32 * (image_width // 4) * (image_height // 4), 256)
         self.fc2 = nn.Linear(256, 1)
 
-    def forward(self, x):
-        x = nn.functional.relu(self.conv1(x))
+        self.relu = nn.ReLU()
+
+    def forward(self, input):
+        out = self.conv1(input)
         out = self.batch_norm1(out)
-        x = nn.functional.relu(self.conv2(x))
-        x = x.view(-1, 32 * (self.image_width // 4) * (self.image_height // 4))
-        x = nn.functional.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.relu(out)
+        
+        out = out.view(-1, 32 * (self.image_width // 4) * (self.image_height // 4))
+        out = self.fc1(out)
+        out = self.relu(out)
+        out = self.fc2(out)
+        return out
