ca601407e241f65afc97bb3af57f8da9ecad0dff
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-09-27 13:59:30 +0300

    Add batch norm Stride instead of max pooling

--- reduce_model.py
+++ reduce_model.py
@@ -8,18 +8,17 @@ class FaceDetectionModel(nn.Module):
         self.image_width = image_width
         self.image_height = image_height
         
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
+        self.batch_norm1 = nn.BatchNorm2d(units)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
         self.fc1 = nn.Linear(32 * (image_width // 4) * (image_height // 4), 256)
         self.fc2 = nn.Linear(256, 1)
 
     def forward(self, x):
         x = nn.functional.relu(self.conv1(x))
-        x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
+        out = self.batch_norm1(out)
         x = nn.functional.relu(self.conv2(x))
-        x = nn.functional.max_pool2d(x, kernel_size=2, stride=2)
         x = x.view(-1, 32 * (self.image_width // 4) * (self.image_height // 4))
         x = nn.functional.relu(self.fc1(x))
         x = self.fc2(x)
         return x
-        
