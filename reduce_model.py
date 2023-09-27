b636ebdbae55a6b86d7e9f443b81ba4d17a106bd
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-09-27 21:11:03 +0300

    Scale reduce model with units parameter

--- reduce_model.py
+++ reduce_model.py
@@ -3,17 +3,17 @@ import torch.nn as nn
 
 
 class FaceDetectionModel(nn.Module):
-    def __init__(self, image_width, image_height):
+    def __init__(self, image_width, image_height, units = 16):
         super(FaceDetectionModel, self).__init__()
         
         self.image_width = image_width
         self.image_height = image_height
         
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1)
-        self.batch_norm1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
-        self.fc1 = nn.Linear(32 * (image_width // 4) * (image_height // 4), 256)
-        self.fc2 = nn.Linear(256, 1)
+        self.conv1 = nn.Conv2d(1, units, kernel_size=3, stride=2, padding=1)
+        self.batch_norm1 = nn.BatchNorm2d(units)
+        self.conv2 = nn.Conv2d(units, 2*units, kernel_size=3, stride=2, padding=1)
+        self.fc1 = nn.Linear(2 * units * (image_width // 4) * (image_height // 4), 16*units)
+        self.fc2 = nn.Linear(16*units, 1)
 
         self.relu = nn.ReLU()
 
