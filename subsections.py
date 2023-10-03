33a39bf2d548d477116b9cee96c2c0a60ab4ed60
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-10-03 19:26:30 +0300

    Use preprocessed data in training

--- subsections.py
+++ subsections.py
@@ -18,8 +18,8 @@ region_step_fraction = 0.1
 keep_fraction = 0.01
 label_keep_fraction = 1
 negatives_per_positive = 5
-save_every = 100
-num_epochs = 10001
+save_every = 10
+num_epochs = 2001
 units = 8
 
 print("units:", units)
@@ -33,132 +33,12 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 print("device:", device)
 
 
-def batch_data(data):
-    arrays = []
-    batches = []
-
-    for i, array in enumerate(data):
-        arrays.append(array)
-        if len(arrays) == batch_size or i == (len(data)-1):
-            arrays_tensor = torch.tensor(np.array(arrays), dtype=torch.float32).to(device)
-            batches.append(arrays_tensor)
-            arrays = []
-
-    return batches
-
-
-
-def load_npy_files(folder_path, validation_fraction=0.1):
-    data = {}
-    
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith('.npy'):
-            words = file_name.split('_')
-            frame_index = int(words[-1].replace(".npy",""))
-            video_name = "_".join(words[:-1])
-            file_path = os.path.join(folder_path, file_name)
-
-            array = np.load(file_path)
-            json_file_path = os.path.join(folder_path, file_name.replace(".npy",".json"))
-            with open(json_file_path, 'r') as f:
-                json_data = json.load(f)
-
-            file_path = os.path.join(folder_path, file_name)
-            if video_name in data:
-                data[video_name].append((frame_index, array, json_data["labels"]))
-            else:
-                data[video_name] = [
-                    (frame_index, array, json_data["labels"]),
-                ]
-
-    train_data_by_resolution = {}
-    valid_data_by_resolution = {}
-
-    for video_name in data.keys():
-        data[video_name].sort(key=lambda x: x[0])
-
-    for video_name, frames in data.items():
-        if len(frames) > 2:
-            num_frames = len(frames)
-            resolution = frames[0][1].shape
-            start_frame = int(np.floor((0.5-0.5*validation_fraction)*num_frames))
-            end_frame = int(np.ceil((0.5+0.5*validation_fraction)*num_frames))
-
-            valid_frames = frames[start_frame:end_frame]
-            train_frames = frames[:start_frame] + frames[end_frame:]
-
-            if resolution not in train_data_by_resolution:
-                train_data_by_resolution[resolution] = train_frames
-            else:
-                train_data_by_resolution[resolution] += train_frames
-
-            if resolution not in valid_data_by_resolution:
-                valid_data_by_resolution[resolution] = valid_frames
-            else:
-                valid_data_by_resolution[resolution] += valid_frames
-
-    train_data_positive = []
-    train_data_negative = []
-    valid_data_positive = []
-    valid_data_negative = []
-
-    print("Creating subregions")
-    for resolution in train_data_by_resolution:
-        for frame_index, array, json_data in train_data_by_resolution[resolution]:
-            labels = [l for l in json_data if l['l'] == 1]
-            if len(labels) == 0:
-                continue
-
-            regions = extract_subregions(
-                array, labels, region_size, region_size, region_step_fraction
-            )
-            for region in regions:
-                if region[1]:
-                    if np.random.choice([True, False], 1, p=[label_keep_fraction,1-label_keep_fraction]):
-                        train_data_positive.append(region[0])
-                else:
-                    if np.random.choice([True, False], 1, p=[keep_fraction,1-keep_fraction]):
-                        train_data_negative.append(region[0])
-
-            #boxes = [(r[2], r[3], region_size, region_size) for r in regions if r[1] > 0.5]
-            #plot_boxes_on_image(array, boxes)
-
-
-    for resolution in valid_data_by_resolution:
-        for frame_index, array, json_data in valid_data_by_resolution[resolution]:
-            labels = [l for l in json_data if l['l'] == 1]
-            if len(labels) == 0:
-                continue
-
-            regions = extract_subregions(
-                array, labels, region_size, region_size, region_step_fraction
-            )
-            for region in regions:
-                if region[1]:
-                    if np.random.choice([True, False], 1, p=[label_keep_fraction,1-label_keep_fraction]):
-                        valid_data_positive.append(region[0])
-                else:
-                    if np.random.choice([True, False], 1, p=[keep_fraction,1-keep_fraction]):
-                        valid_data_negative.append(region[0])
-
-
-    print("Creating batches")
-    random.shuffle(train_data_positive)
-    random.shuffle(train_data_negative)
-    random.shuffle(valid_data_positive)
-    random.shuffle(valid_data_negative)
-    train_data_positive = batch_data(train_data_positive)
-    train_data_negative = batch_data(train_data_negative)
-    valid_data_positive = batch_data(valid_data_positive)
-    valid_data_negative = batch_data(valid_data_negative)
-    
-    return (train_data_positive, train_data_negative), (valid_data_positive, valid_data_negative)
-
-
-
-train_data, valid_data = load_npy_files(data_path)
-print(f"Positive training data: {len(train_data[0])}, negative: {len(train_data[1])}")
-print(f"Positive validation data: {len(valid_data[0])}, negative: {len(valid_data[1])}")
+valid_data_positive = torch.load("valid_data_positive.pt")
+valid_data_negative = torch.load("valid_data_negative.pt")
+train_data_positive = torch.load("train_data_positive.pt")
+train_data_negative = torch.load("train_data_negative.pt")
+print(f"Positive training data: {len(train_data_positive)}, negative: {len(train_data_negative)}")
+print(f"Positive validation data: {len(valid_data_positive)}, negative: {len(valid_data_negative)}")
 
 
 
@@ -168,7 +48,7 @@ loss_function = nn.BCEWithLogitsLoss()
 
 # Cyclical iterator to get batches of negative examples. Helps in balancing
 # positive and negative examples when there are many more negative ones.
-negative_train_data = itertools.cycle(train_data[1])
+negative_train_data = itertools.cycle(train_data_negative)
 
 
 for epoch in range(num_epochs):
@@ -179,7 +59,7 @@ for epoch in range(num_epochs):
     train_accuracy_positive = 0
     train_accuracy_negative = 0
     start_time = time.time()
-    for i, data in enumerate(train_data[0]):
+    for i, data in enumerate(train_data_positive):
         positive_predictions = faceDetector(data)
         loss = loss_function(positive_predictions, torch.ones_like(positive_predictions))
         train_accuracy_positive += nn.functional.sigmoid(positive_predictions).mean().item()
@@ -209,13 +89,13 @@ for epoch in range(num_epochs):
     total_positive = 0
     total_negative = 0
     with torch.no_grad():
-        for data in valid_data[0]:
+        for data in valid_data_positive:
             predictions = faceDetector(data)
             validation_loss += loss_function(predictions, torch.ones_like(predictions)).item()
             validation_accuracy_positive += nn.functional.sigmoid(predictions).mean().item()
             total_positive += 1
         
-        for data in valid_data[1]:
+        for data in valid_data_negative:
             predictions = faceDetector(data)
             validation_loss += loss_function(predictions, torch.zeros_like(predictions)).item()
             validation_accuracy_negative += 1-nn.functional.sigmoid(predictions).mean().item()
