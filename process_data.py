fd95e71ef6e7c24ea7295f08dce22b50ec3981b6
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2023-10-06 15:25:12 +0300

    Script for preprocessing

--- process_data.py
+++ process_data.py
@@ -3,28 +3,25 @@ import numpy as np
 import json
 import torch
 import random
+import click
 
 from subsection_utils import extract_rescaled_subregions
 
-data_path = "data/train_data"
-batch_size = 100
-region_sizes = [32, 48, 64]
-region_step_fraction = 0.1
-keep_fraction = 0.01
-label_keep_fraction = 1
 
-region_size = min(region_sizes)
+
 
 if not os.path.exists("saved"):
     os.makedirs("saved")
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("device:", device)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("device:", device)
 
 
-def batch_data(data):
+def batch_data(data, batch_size):
+
     random.shuffle(data)
     data = torch.tensor(np.array(data, dtype=np.float32), dtype=torch.float32)
+    region_size = data.shape[1]
     n_batches = data.shape[0]//batch_size
     data = data[:n_batches*batch_size]
     batches = torch.reshape(data, [-1, batch_size, region_size, region_size])
@@ -32,24 +29,26 @@ def batch_data(data):
     return batches
 
 
-def load_npy_files(folder_path, validation_fraction=0.1):
+def load_npy_files(data_path, batch_size, region_sizes, step_fraction, keep_fraction, validation_fraction):
     data = {}
     
-    for file_name in os.listdir(folder_path):
+    for file_name in os.listdir(data_path):
         if file_name.endswith('.npy'):
             words = file_name.split('_')
             frame_index = int(words[-1].replace(".npy",""))
             video_name = "_".join(words[:-1])
-            file_path = os.path.join(folder_path, file_name)
+            file_path = os.path.join(data_path, file_name)
 
             array = np.load(file_path)
-            json_file_path = os.path.join(folder_path, file_name.replace(".npy",".json"))
+            json_file_path = os.path.join(data_path, file_name.replace(".npy",".json"))
             with open(json_file_path, 'r') as f:
                 json_data = json.load(f)
 
-            file_path = os.path.join(folder_path, file_name)
+            file_path = os.path.join(data_path, file_name)
             if video_name in data:
                 data[video_name].append((frame_index, array, json_data["labels"]))
+                if len(data[video_name]) > 6:
+                    break
             else:
                 data[video_name] = [
                     (frame_index, array, json_data["labels"]),
@@ -92,14 +91,13 @@ def load_npy_files(folder_path, validation_fraction=0.1):
             labels = [l for l in json_data if l['l'] == 1]
             if len(labels) == 0:
                 continue
-
+            
             regions = extract_rescaled_subregions(
-                array, labels, [32, 48, 64], region_step_fraction
+                array, labels, [32, 48, 64], step_fraction
             )
             for region in regions:
                 if region[1]:
-                    if np.random.choice([True, False], 1, p=[label_keep_fraction,1-label_keep_fraction]):
-                        train_data_positive.append(region[0])
+                    train_data_positive.append(region[0])
                 else:
                     if np.random.choice([True, False], 1, p=[keep_fraction,1-keep_fraction]):
                         train_data_negative.append(region[0])
@@ -110,36 +108,51 @@ def load_npy_files(folder_path, validation_fraction=0.1):
             labels = [l for l in json_data if l['l'] == 1]
             if len(labels) == 0:
                 continue
-
+            
             regions = extract_rescaled_subregions(
-                array, labels, [32, 48, 64], region_step_fraction
+                array, labels, [32, 48, 64], step_fraction
             )
             for region in regions:
                 if region[1]:
-                    if np.random.choice([True, False], 1, p=[label_keep_fraction,1-label_keep_fraction]):
-                        valid_data_positive.append(region[0])
+                    valid_data_positive.append(region[0])
                 else:
                     if np.random.choice([True, False], 1, p=[keep_fraction,1-keep_fraction]):
                         valid_data_negative.append(region[0])
 
 
     print("Creating batches")
-    train_data_positive = batch_data(train_data_positive)
-    train_data_negative = batch_data(train_data_negative)
-    valid_data_positive = batch_data(valid_data_positive)
-    valid_data_negative = batch_data(valid_data_negative)    
+    train_data_positive = batch_data(train_data_positive, batch_size)
+    train_data_negative = batch_data(train_data_negative, batch_size)
+    valid_data_positive = batch_data(valid_data_positive, batch_size)
+    valid_data_negative = batch_data(valid_data_negative, batch_size)
     
     return train_data_positive, train_data_negative, valid_data_positive, valid_data_negative
 
 
 
-train_data_positive, train_data_negative, valid_data_positive, valid_data_negative = load_npy_files(data_path)
 
-print(f"Positive training data: {train_data_positive.shape}, negative: {train_data_negative.shape}")
-print(f"Positive validation data: {valid_data_positive.shape}, negative: {valid_data_negative.shape}")
+@click.command()
+@click.option("--data_path", help="Path to the data folder.", required=True)
+@click.option("--batch_size", default=100, help="Size of the training batches.")
+@click.option("--region_sizes", default=[32,48,64], help="List of bounding box sizes.", multiple=True)
+@click.option("--step_fraction", default=0.1, help="Step between bounding boxes as a fraction of its size.")
+@click.option("--keep_fraction", default=0.01, help="Fraction of negative data to keep.")
+@click.option("--validation_fraction", default=0.1, help="Fraction of negative data to keep.")
+def preprocess_images(data_path, batch_size, region_sizes, step_fraction, keep_fraction, validation_fraction):
 
-torch.save(train_data_positive, 'train_data_positive.pt')
-torch.save(train_data_negative, 'train_data_negative.pt')
-torch.save(valid_data_positive, 'valid_data_positive.pt')
-torch.save(valid_data_negative, 'valid_data_negative.pt')
+    train_data_positive, train_data_negative, valid_data_positive, valid_data_negative = load_npy_files(data_path, batch_size, region_sizes, step_fraction, keep_fraction, validation_fraction)
+
+    print(f"Positive training data: {train_data_positive.shape}, negative: {train_data_negative.shape}")
+    print(f"Positive validation data: {valid_data_positive.shape}, negative: {valid_data_negative.shape}")
+
+    torch.save(train_data_positive, 'train_data_positive.pt')
+    torch.save(train_data_negative, 'train_data_negative.pt')
+    torch.save(valid_data_positive, 'valid_data_positive.pt')
+    torch.save(valid_data_negative, 'valid_data_negative.pt')
+
+
+
+if __name__ == '__main__':
+    preprocess_images()
+
 
